e5354ae8e675f8f782f1636bd0d22fd083ec62a0
Ujjwal <ujjwal2282@gmail.com>
2026-01-06 22:27:59 +0530

    FRONTEND/JAVA SCRIPT 2

---
+++ FRONTEND/JAVA SCRIPT 2/app.js
@@ -0,0 +1,14 @@
+console.log("Hello World!");
+console.log("ujjwal");
+let a=10;
+let b=5;
+console.log("sum is:",a+b);  
+
+let pencilPrice=10;
+let earaserPrice=5;
+console.log(`The Total Price: ${pencilPrice+earaserPrice} Rupees.`);
+
+
+alert("something is wrong!");
+var str=prompt("please enter your name");
+alert(`your name is ${str}`);
